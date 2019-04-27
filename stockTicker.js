421588b0e95ac9a01cf0181d1d33802e1e718fc2
Prashant <bhandariprashant616@gmail.com>
2019-04-27 17:04:35 -0500

    Added functionality to draw graph from api data

--- stockTicker.js
+++ stockTicker.js
@@ -21,31 +21,97 @@ $(function () {
     $('form').on('keyup', '.tickerSearch', function (event) {
         ImplementTickerAutoComplete($(this).val());
     })
-    $('form').on('click','button',function(evnt){
-    //evnt.preventDefault();
-    //console.log(`${$('.tickerSearch').val()}`)
-    $('.Result').html(`<a href="#">${$('.tickerSearch').val()}</a>`);
+    $('form').on('click', 'button', function (evnt) {
+        evnt.preventDefault();
+        $('.Result').html(`<a href="#">${$('.tickerSearch').val()}</a>`);
     });
-    function GetDataFromUnibitApi()
-    {
-        let url = `https://api.unibit.ai/historicalstockprice/${$('.tickerSearch')}`;
-        let queryParam = 
+    $('.Result').on('click', 'a', function (evt) {
+        evt.preventDefault();
+        GetDataFromUnibitApi();
+    });
+    function GetDataFromUnibitApi() {
+        let url = `https://api.unibit.ai/historicalstockprice/${$('.tickerSearch').val()}`;
+        let queryParam =
         {
-            ticker:$('.tickerSearch').val(),
-           range:$('.dataDuration').val(),
-           interval:3,
-           AccessKey:'hf8cQehsukDBW8oWQHvUTPDGYP8RAjns'
+            range: $('.dataDuration').val(),
+            interval: 3,
+            AccessKey: 'hf8cQehsukDBW8oWQHvUTPDGYP8RAjns'
 
         }
+        let urlQString = GetQueryString(queryParam);
+        url = url + '?' + urlQString;
+        console.log(url);
+        fetch(url)
+            .then(r => r.json().then(data => ({
+                status: r.status, body: data
+            })))
+            .then(f => {
+                let stockMktObj = (f.body["Stock price"]);
+                //console.log(stockMktObj.length);
+                let dateData = [];
+                let highValData = [];
+                let lowValData = [];
+                let dataLength = stockMktObj.length
+                for (let i = 0; i < dataLength; i++) 
+                {
+                    dateData.push(stockMktObj[i].date);
+                    highValData.push(stockMktObj[i].high);
+                    lowValData.push(stockMktObj[i].low)
+                    //console.log(stockMktObj[i]);
+                }
+                var trace1 = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: 'AAPL High',
+                    x: dateData,
+                    y: highValData,
+                    line: { color: '#17BECF' }
+                }
 
-       
+                var trace2 = {
+                    type: "scatter",
+                    mode: "lines",
+                    name: 'AAPL Low',
+                    x: dateData,
+                    y: lowValData,
+                    line: { color: '#7F7F7F' }
+                }
 
+                var data = [trace1, trace2];
+
+                var layout = {
+                    title: 'Basic Time Series',
+                };
+
+                Plotly.newPlot('GraphResult', data, layout);
+
+            })
+
+            .catch(err => {
+                $('#GraphResult').html(`Error Occurred : ${err.message}`)
+            });
 
     }
-    function GetQueryString(parameters)
-    {
+
+    // function ShowTimeSeriesGraph(jsonResult) {
+    //     let receivedJson =  {};
+    //     receivedJson = jsonResult;
+    //     console.log(`Received JSON : ${receivedJson}`);
+    //     let dateData = [];
+    //     let highValData = [];
+    //     let lowValData = [];
+    //     // for(let i=0; i<jsonResult.lenght;i++)
+    //     // {
+    //     //     dateData.push(jsonResult[i].date);
+    //     //     highValData.push(jsonResult[i].high);
+    //     //     lowValData.push(jsonResult[i].low)
+    //     // }
+
+    //     //console.log(`Json Data: ${jsonResult} , DateData:${dateData} , HighData :${highValData} , LowData : ${lowValData} `);
+    // }
+    function GetQueryString(parameters) {
         let keys = Object.keys(parameters);
-        let queryItems= keys.map(k=>`${k}=${parameters[k]}`);
-         return queryItems.join('&');
+        let queryItems = keys.map(k => `${k}=${parameters[k]}`);
+        return queryItems.join('&');
     }
 });
