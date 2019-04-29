ae03cf86c410c6aff2e5f69f46b7f93e15bb1f27
Audrey Foss <porcupine4@gmail.com>
2019-04-28 21:59:33 -0500

    formatted some js and got it working again

--- stockTicker.js
+++ stockTicker.js
@@ -1,117 +1,126 @@
 const STORE = {
-  
-
+    unibitKeyPrashant: 'GkIYaHmIBTNnbr_fAsSnhnAhp4FF85tE', /* cspell: disable-line */
+    unibitKeyAudrey: 'oa1yyhplyzggaowa88stunvzislwdnvkqg1oz8qf',
+    unibitHistoricalUrl: 'https://api.unibit.ai/historicalstockprice/',
+    queryParamForGraph: {
+        range: $('.dataDuration').val(),
+        interval: 3,
+        AccessKey: 'GkIYaHmIBTNnbr_fAsSnhnAhp4FF85tE'}, /* cspell: disable-line */
+    
 };
 
-$(function () {
-    function ImplementTickerAutoComplete(searchText) {
-        let tickerLst = [{ value: "A", label: "A" },
-        { value: "AA", label: "AA" },
-        { value: "AAC", label: "AAC" },
-        { value: "AAPL", label: "AAPL" },
-        { value: "GOOG", label: "GOOG" },
-        { value: "MSFT", label: "MSFT" },
-        { value: "CTBK", label: "CTBK" },
-        { value: "JPM", label: "JPM" },
-        { value: "BAC", label: "BAC" },
-        { value: "TSLA", label: "TSLA" },
-        { value: "LEN", label: "LEN" },
-        { value: "KBH", label: "KBH" },
-        { value: "PRU", label: "PRU" }
-        ]
-        $('.tickerSearch').autocomplete({
-            source: tickerLst
-        });
-    }
-    $('form').on('keyup', '.tickerSearch', function (event) {
-        ImplementTickerAutoComplete($(this).val());
-    })
-    $('form').on('click', 'button', function (evnt) {
-        evnt.preventDefault();
-        $('.Result').html(`<a href="#">${$('.tickerSearch').val()}</a>`);
+function implementTickerAutoComplete(searchText) {
+    let tickerList = [{ value: "A", label: "A" },
+    { value: "AA", label: "AA" },
+    { value: "AAC", label: "AAC" },
+    { value: "AAPL", label: "AAPL" },
+    { value: "GOOG", label: "GOOG" },
+    { value: "MSFT", label: "MSFT" },
+    { value: "CTBK", label: "CTBK" },
+    { value: "JPM", label: "JPM" },
+    { value: "BAC", label: "BAC" },
+    { value: "TSLA", label: "TSLA" },
+    { value: "LEN", label: "LEN" },
+    { value: "KBH", label: "KBH" },
+    { value: "PRU", label: "PRU" }
+    ];
+    $('.tickerSearch').autocomplete({
+        source: tickerList
     });
-    $('.Result').on('click', 'a', function (evt) {
-        evt.preventDefault();
-        GetDataFromUnibitApi();
-    });
-    function GetDataFromUnibitApi() {
-        let url = `https://api.unibit.ai/historicalstockprice/${$('.tickerSearch').val()}`;
-        let queryParam =
-        {
-            range: $('.dataDuration').val(),
-            interval: 3,
-            AccessKey: 'GkIYaHmIBTNnbr_fAsSnhnAhp4FF85tE'
-
+}
+// Event delegator - called at bottom
+function startTicking() {
+    $('form').on('keyup', '.tickerSearch', function(event) {
+        implementTickerAutoComplete($(this).val());
         }
-        let urlQString = GetQueryString(queryParam);
-        url = url + '?' + urlQString;
-        console.log(url);
-        fetch(url)
-            .then(r => r.json().then(data => ({
-                status: r.status, body: data
-            })))
-            .then(f => {
-                if (f.status == 200) {
-                    let stockMktObj = (f.body["Stock price"]);
-                    let dateData = [];
-                    let highValData = [];
-                    let lowValData = [];
-                    let dataLength = stockMktObj.length
-                    for (let i = 0; i < dataLength; i++) {
-                        dateData.push(stockMktObj[i].date);
-                        highValData.push(stockMktObj[i].high);
-                        lowValData.push(stockMktObj[i].low)
-                        //console.log(stockMktObj[i]);
-                    }
-                    var trace1 = {
-                        type: "scatter",
-                        mode: "lines",
-                        name: `${$('.tickerSearch').val()} High`,
-                        x: dateData,
-                        y: highValData,
-                        line: { color: '#17BECF' }
-                    }
-
-                    var trace2 = {
-                        type: "scatter",
-                        mode: "lines",
-                        name: `${$('.tickerSearch').val()} Low`,
-                        x: dateData,
-                        y: lowValData,
-                        line: { color: '#7F7F7F' }
-                    }
-
-                    var data = [trace1, trace2];
-
-                    var layout = {
-                        title: `Time Series Stock Value Variation Of ${$('.tickerSearch').val()} For a Duration Of ${$('.dataDuration').val()}`,
-                    };
-
-                    Plotly.newPlot('GraphResult', data, layout);
-                }
-                else {
-                    $('#GraphResult').html(`Error Occurred : ${f.body.message}`);
-                }
-
-            })
-
-            .catch(err => {
-                $('#GraphResult').html(`Error Occurred : ${err.message}`)
-            });
-
+    );
+    $('form').on('click', 'button', function (event) {
+        event.preventDefault();
+        $('.result').html(`<a href="#">${$('.tickerSearch').val()}</a>`);
+    });
+    $('.result').on('click', 'a', function (event) {
+        event.preventDefault();
+        getGraphDataFromUnibitApi();
+    });
+}
+function createApiUrl(queryParam) {
+    let queryParams;
+    if (queryParam == 'graph') {
+        queryParams = STORE.queryParamForGraph;
     }
+    let url = STORE.unibitHistoricalUrl + $('.tickerSearch').val();
+    let urlQString = getQueryString(queryParams);
+    outputUrl = url + '?' + urlQString;
+    console.log(outputUrl);
+    return outputUrl;
+}
+function getQueryString(parameters) {
+    let keys = Object.keys(parameters);
+    let queryItems = keys.map(k => `${k}=${parameters[k]}`);
+    return queryItems.join('&');
+}
+function getGraphDataFromUnibitApi() {
+    let queryParam = 'graph';
+    const graphUrl = createApiUrl(queryParam);
+    fetch(graphUrl)
+    .then(r => r.json().then(data => ({
+        status: r.status, body: data
+    })))
+    .then(f => {
+        if (f.status == 200) {
+            let stockMktObj = (f.body["Stock price"]);
+            let dateData = [];
+            let highValData = [];
+            let lowValData = [];
+            let dataLength = stockMktObj.length;
+            for (let i = 0; i < dataLength; i++) {
+                dateData.push(stockMktObj[i].date);
+                highValData.push(stockMktObj[i].high);
+                lowValData.push(stockMktObj[i].low);
+                //console.log(stockMktObj[i]);
+            }
+            var trace1 = {
+                type: "scatter",
+                mode: "lines",
+                name: `${$('.tickerSearch').val()} High`,
+                x: dateData,
+                y: highValData,
+                line: { color: '#17BECF' }
+            };
 
-    function GetQueryString(parameters) {
-        let keys = Object.keys(parameters);
-        let queryItems = keys.map(k => `${k}=${parameters[k]}`);
-        return queryItems.join('&');
-    }
-});
+            var trace2 = {
+                type: "scatter",
+                mode: "lines",
+                name: `${$('.tickerSearch').val()} Low`,
+                x: dateData,
+                y: lowValData,
+                line: { color: '#7F7F7F' }
+            };
 
+            var data = [trace1, trace2];
 
+            var layout = {
+                title: `Time Series Stock Value Variation Of ${$('.tickerSearch').val()} For a Duration Of ${$('.dataDuration').val()}`,
+            };
+
+            Plotly.newPlot('GraphResult', data, layout); /* cspell: disable-line */
+        }
+        else {
+            $('#GraphResult').html(`Error Occurred : ${f.body.message}`);
+        }
+
+    })
+
+    .catch(err => {
+        $('#GraphResult').html(`Error Occurred : ${err.message}`);
+    });
+}
+
+implementTickerAutoComplete();
+startTicking();
 
 // Audrey's section start
-
+// 
 
 
 
